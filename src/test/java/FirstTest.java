aca210040b25368313e9082e0121cb0bf1070c93
Oleg Bulavitchi <bulavitchi@gmail.com>
2013-06-10 23:18:41 +0100

    build dependencies

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,12 @@
+import junit.framework.Assert;
+import org.junit.Test;
+
+public class FirstTest {
+
+    @Test
+    public void test() {
+         System.out.println("Test created");
+        Assert.assertTrue(true);
+    }
+
+}
